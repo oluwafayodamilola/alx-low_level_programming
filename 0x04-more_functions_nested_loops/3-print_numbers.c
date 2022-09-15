e9f0c535546342966ee0697dae393791115c8d38
adeolaakee <adeolaakee@gmail.com>
2022-09-15 19:11:35 +0000

    getting better by the day

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers(void) - prints from 0 to 9, followed by a new line.
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+		_putchar(ch + '0');
+	_putchar('\n');
+}
