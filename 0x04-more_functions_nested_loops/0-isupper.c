04b575a2332e38d689d9477921c850b458cb31da
adeolaakee <adeolaakee@gmail.com>
2022-09-15 18:29:51 +0000

    uppertest

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - the entry of the program
- * _isupper(intc) - return 1 for uppercase and 0 for lowercase
+ * _isupper - return 1 for uppercase and 0 for lowercase
+ * @c: The character to check
  *
  * Return: 1;
  */
