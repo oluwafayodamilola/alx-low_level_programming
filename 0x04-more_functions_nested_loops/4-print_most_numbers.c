f7985dd97745ea712146cc6b43fb7669b66b1ad0
adeolaakee <adeolaakee@gmail.com>
2022-09-15 19:22:18 +0000

    exclude

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number from 0 t0 9
+ * do not print 2 and 4
+ * Return: nil
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+}
