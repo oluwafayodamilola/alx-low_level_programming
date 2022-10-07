c8e4b393a190e0ab52b9ea74472df41fb936b9b8
adeolaakee <adeolaakee@gmail.com>
2022-10-07 15:01:41 +0000

    going well

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,15 @@
 
 char *create_array(unsigned int size, char c)
 {
-
+	unsigned int i;
 	char *array;
+	
+	i = 0;
 	
 	if (size == 0)
 		return(NULL);
 	array = malloc(sizeof(char) * size);
 
-	int i;
 
 	while (i < size)
 	{
